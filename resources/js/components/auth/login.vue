3d89db2db2a6493027b65e4672a9ef06de6faa2b
codiesinstallation <codiesinstallation@gmail.com>
2022-12-12 23:50:16 +0200

    auto update raqeem installation Code

--- resources/js/components/auth/login.vue
+++ resources/js/components/auth/login.vue
@@ -72,7 +72,7 @@
                                             class="h6 text--lighten-1 alert alert-danger"
                                         >
                                             <span class="mx-2">
-                                                {{ __("Support To ") }}:</span
+                                                {{ __("علاء مرسي") }}:</span
                                             >
                                             {{
                                                 $root.$data.codies
