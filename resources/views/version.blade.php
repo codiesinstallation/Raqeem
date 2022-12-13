fde7e287eb0a3455c6c186b3453cfb550ac375e8
codiesinstallation <codiesinstallation@gmail.com>
2022-12-13 16:57:24 +0200

    auto update raqeem installation Code

--- resources/views/version.blade.php
+++ resources/views/version.blade.php
@@ -1,25 +1,31 @@
 @extends('layout.app')
 
 @section('content')
-    <div class="card col-12" style="overflow-x: scroll">
-        <div class="card-header">
-            <h6>رقيم</h6>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
 
-        </div>
-        <div class="body">
-            @if (str_contains($res, 'nothing to commit') || str_contains($res, 'no changes added to commit'))
-                <div class="alert alert-success" role="alert">
-                    اخر أصدار
+                <div class="card ">
+                    <div class="card-header">
+                        <h6>رقيم</h6>
+
+                    </div>
+                    <div class="body text-center">
+                        @if (str_contains($res, 'nothing to commit') || str_contains($res, 'no changes added to commit'))
+                            <div class="alert alert-success" role="alert">
+                                اخر أصدار
+                            </div>
+                        @else
+                            <div class="alert alert-warning" role="alert">
+                                هناك تحديث متاح
+                            </div>
+                            <a href="{{ route('updated') }}"> تحديث
+                            </a>
+                        @endif
+
+                    </div>
+
                 </div>
-            @else
-               <div class="alert alert-warning" role="alert">
-                    هناك تحديث متاح
-                </div>
-                <a href="{{ route('updated') }}"> تحديث
-            </a>
-            @endif
-
+            </div>
         </div>
-
-    </div>
-@endsection
+    @endsection
