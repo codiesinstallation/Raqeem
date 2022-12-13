55a258fbaf1bb4e11036eb13f9a111537d2f6ef7
codiesinstallation <codiesinstallation@gmail.com>
2022-12-13 17:08:41 +0200

    auto update raqeem installation Code

--- resources/views/version.blade.php
+++ resources/views/version.blade.php
@@ -11,9 +11,9 @@
 
                     </div>
                     <div class="body text-center">
-                        @if (str_contains($res, 'nothing to commit') || str_contains($res, 'no changes added to commit'))
+                        @if (Str::startsWith($res, 'nothing to commit') || Str::startsWith($res, 'no changes added to commit'))
                             <div class="alert alert-success" role="alert">
-                              ?  اخر أصدار
+                                اخر أصدار
                             </div>
                         @else
                             <div class="alert alert-warning" role="alert">
