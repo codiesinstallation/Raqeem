f553218b91d1ecd6e6251f7996f895a649ea5f6d
codiesinstallation <codiesinstallation@gmail.com>
2022-12-17 21:48:33 +0200

    auto update raqeem installation Code

--- backup/last.sql
+++ backup/last.sql
@@ -1,5 +1,9 @@
 
 ALTER TABLE
+    `raqeem`.`type_units` DROP FOREIGN KEY `tu_type_id`,
+    DROP FOREIGN KEY `uu_unit_id`;
+
+ALTER TABLE
     `raqeem`.`type_units`
 ADD
     CONSTRAINT `tu_type_id` FOREIGN KEY (`type_id`) REFERENCES `raqeem`.`types` (`type_id`) ON DELETE CASCADE ON UPDATE CASCADE,
