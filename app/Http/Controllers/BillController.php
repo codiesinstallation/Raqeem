2413cd7126765b1a75b2c87cc21bb1d914bfe4b9
Mostafa <m.abdalmoamen@gmail.com>
2023-01-02 19:54:43 +0200

    Mostafa

--- app/Http/Controllers/BillController.php
+++ app/Http/Controllers/BillController.php
@@ -1,3 +1,65 @@
 <?php
-bolt_decrypt( __FILE__ , '5f25ou'); return 0;
-##!!!##8/Dz8FRHU0tZVkdJSwYnVlZCLlpaVkIpVVRaWFVSUktYWSHz8PPwW1lLBidWVkIqR1pHOkdIUktZQihPUlJZKkdaRzpHSFJLIfPwW1lLBidWVkIqR1pHOkdIUktZQjpfVksqR1pHOkdIUksh8/BbWUsGJ1ZWQipHWkc6R0hSS1lCO1lLWFkqR1pHOkdIUksh8/BbWUsGJ1ZWQjNVSktSWUIpW1laVVNLWCHz8FtZSwYnVlZCM1VKS1JZQjNPXk9UWSHz8FtZSwYnVlZCM1VKS1JZQjZHXzNLWk5VSlkh8/BbWUsGJ1ZWQjNVSktSWUI6X1ZLIfPwW1lLBidWVkIzVUpLUllCO1lLWCHz8FtZSwYvUlJbU09UR1pLQi5aWlZCOEtXW0tZWiHz8FtZSwYvUlJbU09UR1pLQjlbVlZVWFpCLEdJR0pLWUIqKCHz8PPwSVJHWVkGKE9SUilVVFpYVVJSS1gGS15aS1RKWQYpVVRaWFVSUktY8/Bh8/AGBgYGVltIUk9JBkxbVElaT1VUBk9USkteDihPUlJZKkdaRzpHSFJLBgpKR1pHOkdIUksP8/AGBgYGYfPw8/AGBgYGBgYGBk9MBg4KSkdaRzpHSFJLEyRXW0tYXw4PDwZh8/AGBgYGBgYGBgYGBgZYS1pbWFQGCkpHWkc6R0hSS/PwBgYGBgYGBgYGBgYGBgYGBhMkWEtUSktYDg1IT1JSWQ0SBkENU09eT1RZDQYjJAYzT15PVFkgIF1OS1hLDg1PSg0SBhcPEyRMT1hZWg4PEgYNSE9SUkVaVVpHUg0GIyQGCkpHWkc6R0hSSxMkV1tLWF8ODxMkWVtTDg1IT1JSRVpVWkdSDQ8SBg1ZW1MNBiMkBgpKR1pHOkdIUksTJFdbS1hfDg8TJFlbUw4NSE9SUkVZW1MNDxIGDVxHWg0GIyQGCkpHWkc6R0hSSxMkV1tLWF8ODxMkWVtTDg1IT1JSRVxHWkVcR1INDxIGDVZHX1kNBiMkBjZHXzNLWk5VSlkgIE1LWg4PEgYNSVtZWlVTS1hZDQYjJAYpW1laVVNLWCAgTUtaDg8SBg1bWUtYWQ0GIyQGO1lLWCAgXU5LWEsODU9KDRIGDQcjDRIGExcPEyRNS1oOD0MPIfPwBgYGBgYGBgZj8/AGBgYGBgYGBlhLWltYVAYKSkdaRzpHSFJL8/AGBgYGBgYGBgYGBgYTJFhLVEpLWA4NSE9SUlkNEgZBDVNPXk9UWQ0GIyQGM09eT1RZICBdTktYSw4NT0oNEgYXDxMkTE9YWVoODxIGDVZHX1kNBiMkBjZHXzNLWk5VSlkgIE1LWg4PEgYNSVtZWlVTS1hZDQYjJAYpW1laVVNLWCAgTUtaDg8SBg1bWUtYWQ0GIyQGO1lLWCAgXU5LWEsODU9KDRIGDQcjDRIGExcPEyRNS1oOD0MPIfPwBgYGBmPz8AYGBgZWW0hST0kGTFtUSVpPVVQGT1pLUw4PYfPwBgYGBgYGBgZYS1pbWFQGWEtZVlVUWUsODxMkUFlVVA46X1ZLICBdT1pODg1aX1ZLOVpVSVENEgYNW1RPWlkNDxMkXU5LWEsODU9ZRUpLUktaS0oNEgZMR1JZSw8TJE1LWg4PDyHz8PPwBgYGBmPz8AYGBgZWW0hST0kGTFtUSVpPVVQGW1ZKR1pLDg/z8AYGBgZh8/AGBgYGBgYGBlhLWltYVAZcT0tdDg1cS1hZT1VUDQ8h8/AGBgYGY/PwBgYGBlZbSFJPSQZMW1RJWk9VVAZbVkpHWktKDg/z8AYGBgZh8/AGBgYGBgYGBgpTT15PVFkGIwYzT15PVFkgIF1OS1hLDg1PSg0SBhcPEyRMT1hZWg4PIfPwBgYGBgYGBgYKWVpHWFoqR1pLBiMGWVpYWlVaT1NLDkpHWksODT8TUxNKDRIGWVpYWlVaT1NLDgpTT15PVFkTJEtUSkVZW1ZWVVhaRUpHWksPDw8h8/AGBgYGBgYGBgpJW1hYS1RaKkdaSwYjBllaWFpVWk9TSw5KR1pLDg0/E1MTSg0PDyHz8PPwBgYGBgYGBgZPTAYOCllaR1haKkdaSwYkBgpJW1hYS1RaKkdaSw8GYfPwBgYGBgYGBgYGBgYGS15LSQ4ITU9aBlZbUlIGDAwGSVVTVlVZS1gGW1ZKR1pLBhMTT01UVVhLE1ZSR1pMVVhTE1hLVyNLXloTSFVSWgYMDAZUVlMGT1RZWkdSUgYMDAZUVlMGWFtUBlZYVUoGCA8h8/AGBgYGBgYGBgYGBgYKWEtZBiMGDb6Qv2sGvo2/ar6QvpO+lb9wvpEGvo6/bL6Svo2+kw0h8/AGBgYGBgYGBgYGBgYKVkdaTgYjBkhHWUtFVkdaTg4PBhQGDUJHVlZCSEdJUVtWQlJHWVoUWVdSDSHz8AYGBgYGBgYGBgYGBgpZV1IGIwZMT1JLRU1LWkVJVVRaS1RaWQ4KVkdaTg8h8/Dz8AYGBgYGBgYGBgYGBlpYXwZh8/AGBgYGBgYGBgYGBgYGBgYGKiggIFtUVlhLVkdYS0oOCllXUg8h8/AGBgYGBgYGBgYGBgZjBklHWklOBg5CL1JSW1NPVEdaS0IqR1pHSEdZS0I3W0tYXyteSUtWWk9VVAYKS14PBmHz8AYGBgYGBgYGBgYGBgYGBgYVFQZKSg4KS14TJE1LWjNLWVlHTUsODw8h8/AGBgYGBgYGBgYGBgZj8/AGBgYGBgYGBmMGS1JZSwZh8/AGBgYGBgYGBgYGBgYKWEtZBiMGCL9wvpe+kr9vBr6Nv2q+kL9uvo2+m79qBr9rvp8Gvo2/ar6Vvp+/awa/ar9qv2e/bL9wBr9qvpC+kr6Vv3C+lQa+jb9qvpW+n79rBr9uvo2/ar6QvpO+lb9wvpEIIfPwBgYGBgYGBgZj8/Dz8AYGBgYGBgYGWEtaW1hUBlxPS10ODVxLWFlPVVQNDxMkXU9aTg4NWEtZDRIGClhLWQ8h8/AGBgYGY/PwBgYGBlZbSFJPSQZMW1RJWk9VVAZaX1ZLWQ46X1ZLKkdaRzpHSFJLBgpKR1pHOkdIUksP8/AGBgYGYfPw8/AGBgYGBgYGBlhLWltYVAYKSkdaRzpHSFJL8/AGBgYGBgYGBgYGBgYTJFhLVEpLWA4NWl9WS1k6VSteSUtSDQ8h8/AGBgYGY/PwY/Pw
+
+namespace App\Http\Controllers;
+
+use App\DataTables\BillsDataTable;
+use App\DataTables\TypeDataTable;
+use App\DataTables\UsersDataTable;
+use App\Models\Customer;
+use App\Models\Mixins;
+use App\Models\PayMethods;
+use App\Models\Type;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class BillController extends Controller
+{
+    public function index(BillsDataTable $dataTable)
+    {
+
+        if ($dataTable->query()) {
+            return $dataTable
+                ->render('bills', ['mixins' => Mixins::where('id', 1)->first(), 'bill_total' => $dataTable->query()->sum('bill_total'), 'sum' => $dataTable->query()->sum('bill_sum'), 'vat' => $dataTable->query()->sum('bill_vat_val'), 'pays' => PayMethods::get(), 'customers' => Customer::get(), 'users' => User::where('id', '!=', -1)->get()]);
+        }
+        return $dataTable
+            ->render('bills', ['mixins' => Mixins::where('id', 1)->first(), 'pays' => PayMethods::get(), 'customers' => Customer::get(), 'users' => User::where('id', '!=', -1)->get()]);
+    }
+    public function item(){
+        return response()->json(Type::with('typeStock', 'units')->where('is_deleted', false)->get());
+
+    }
+    public function update()
+    {
+        return view('version');
+    }
+    public function updated()
+    {
+        $mixins = Mixins::where('id', 1)->first();
+        $startDate = strtotime(date('Y-m-d', strtotime($mixins->end_support_date)));
+        $currentDate = strtotime(date('Y-m-d'));
+
+        if ($startDate > $currentDate) {
+            exec("git reset --hard && git pull && composer update --ignore-platform-req=ext-bolt && npm install && npm run prod ");
+            $res = 'تم التحديث بنجاح';
+            $path = base_path() . '\app\backup\last.sql';
+            $sql = file_get_contents($path);
+
+            try {
+                DB::unprepared($sql);
+            } catch (\Illuminate\Database\QueryException $ex) {
+                // dd($ex->getMessage());
+            }
+        } else {
+            $res = "يرجى التواصل مع الدعم للفني لتجديد الدعم والتحديث";
+        }
+
+        return view('version')->with('res', $res);
+    }
+    public function types(TypeDataTable $dataTable)
+    {
+
+        return $dataTable
+            ->render('typesToExcel');
+    }
+}
